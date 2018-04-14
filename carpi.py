aaf560b9f5ae1c42b425fbc6edf734ec61a5969b
Zach Brogan <zachbrogan@gmail.com>
2018-04-13 23:25:07 -0600

    Add GPIO calls

--- carpi.py
+++ carpi.py
@@ -1,8 +1,27 @@
 import subprocess
+import RPi.GPIO as GPIO
 from time import sleep
-# Add gpio imports
+
+
+locked = True # Current state of vehicle: doors locked (T) or doors unlocked (F)
+absent_count = 0 # I may remove these two variables if they are not needed for reliability
+ABSENT_COUNT_MAX = 1
+LOCK_PIN = 32
+UNLOCK_PIN = 26
+TRUNK_PIN = 38
+PANIC_PIN = 40
+BT_ADDRESSES = ['1C:5C:F2:AB:FA:78', 'B4:4B:D2:38:3A:C3'] #List of
+
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(LOCK_PIN, GPIO.OUT)
+GPIO.setup(UNLOCK_PIN, GPIO.OUT)
+GPIO.output(LOCK_PIN, GPIO.LOW)
+GPIO.output(UNLOCK_PIN, GPIO.LOW)
+
 
 def inProximity():
+    # found = False
+    # for bt_addr in BT_ADDRESSES:
     try:
         resp = subprocess.check_output("sudo hcitool cc 1C:5C:F2:AB:FA:78; sudo hcitool rssi 1C:5C:F2:AB:FA:78", shell=True)
         print(resp)
@@ -16,27 +35,33 @@ def inProximity():
     except:
         # Not found
         return False
-    
+    #return found
 
 def lock():
     #Add GPIO on/off calls here
-    subprocess.call("sudo bash -c 'echo 1 >/sys/class/leds/led0/brightness'", shell= True)
+    #subprocess.call("sudo bash -c 'echo 1 >/sys/class/leds/led0/brightness'", shell= True)
     global locked
     locked = True
     print("Locking")
+    GPIO.output(LOCK_PIN, GPIO.HIGH)
+    sleep(0.1)
+    GPIO.output(LOCK_PIN, GPIO.LOW)
 
 
 def unlock():
     #Add GPIO on/off calls here
-    subprocess.call("sudo bash -c 'echo 0 >/sys/class/leds/led0/brightness'", shell= True)
+    #subprocess.call("sudo bash -c 'echo 0 >/sys/class/leds/led0/brightness'", shell= True)
     global locked
     locked = False
     print("Unlocking")
+    GPIO.output(UNLOCK_PIN, GPIO.HIGH)
+    sleep(0.1)
+    GPIO.output(UNLOCK_PIN, GPIO.LOW)
+    sleep(0.5)
+    GPIO.output(UNLOCK_PIN, GPIO.HIGH)
+    sleep(0.1)
+    GPIO.output(UNLOCK_PIN, GPIO.LOW)
 
-
-locked = True # Current state of vehicle: doors locked (T) or doors unlocked (F)
-absent_count = 0 # I may remove these two variables if they are not needed for reliability
-ABSENT_COUNT_MAX = 1
 
 while(True):
     # Device is in range, so car should be unlocked
